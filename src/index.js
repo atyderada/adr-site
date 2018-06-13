e1f4aea9b5f4c7e767c20f0620f7ca41bb7543e4
Anthony De Rada <anthonyderada@gmail.com>
2018-06-13 12:47:34 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
